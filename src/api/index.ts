023c6b033206ab7b25a4eeb8396f3f47d35c862c
Miquéias Castro Soares <107892315+miqueiassoares@users.noreply.github.com>
2023-09-16 10:44:26 -0300

    feat: random book list

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,28 @@ import axios from 'axios';
 
 const key = process.env.NEXT_API_KEY;
 
+const getBookCategory = () => {
+  const anyNumber = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+  switch (anyNumber) {
+    case 1:
+      return 'harrypotter'
+    case 2:
+      return 'gameofthrones'
+    case 3:
+      return 'bigbang'
+    default:
+      return "books"
+  }
+}
+
 const api = axios.create({
   baseURL: process.env.NEXT_BASE_URL
 });
 
 export const getSuggestions = async () => {
+  const books = getBookCategory();
   const data = await api
-    .get(`/volumes?q=harrypotter`)
+    .get(`/volumes?q=${books}`)
     .then((response) => response.data)
     .catch((error) => error);
   return data.items;
